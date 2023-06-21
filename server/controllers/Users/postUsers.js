8e36d0b6c945e23a5e06217807abd5034f56d618
StrafeYosef <yosefplayz4580@gmail.com>
2023-06-21 13:45:40 +0300

    t

--- server/controllers/Users/postUsers.js
+++ server/controllers/Users/postUsers.js
@@ -1,5 +1,6 @@
 const Users = require("../../models/userModel");
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 require("dotenv").config();
 
 const postUsers = async (req, res) => {
@@ -13,7 +14,8 @@ const postUsers = async (req, res) => {
     const token = jwt.sign(itemsToken, secretToken, {
       expiresIn: "1d",
     });
-    const newUser = await Users.create({ name, password, token });
+    const protectedPassword = bcrypt.hash(password, 10);
+    const newUser = await Users.create({ name, password: protectedPassword, token });
     await newUser.save();
     res.status(200).json({ newUser });
   } catch (error) {
