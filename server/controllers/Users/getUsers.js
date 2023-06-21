82a518c576a501845ddb5c5c4655a4463a1df86d
StrafeYosef <yosefplayz4580@gmail.com>
2023-06-21 12:42:14 +0300

    l

--- server/controllers/Users/getUsers.js
+++ server/controllers/Users/getUsers.js
@@ -1,13 +1,14 @@
 const Users = require('../../models/userModel');
 
 const getUsers = async(req, res) => {
-    try {
-        const getUser = await getUsers.find({})
-        return res.status(200).json(getUser);
+    res.json({msg: 'hh'})
+    // try {
+    //     const getUser = await getUsers.find({})
+    //     return res.status(200).json(getUser);
         
-    } catch (error) {
-        console.log(error)
-    }
+    // } catch (error) {
+    //     console.log(error)
+    // }
 } 
 
 module.exports = getUsers;
