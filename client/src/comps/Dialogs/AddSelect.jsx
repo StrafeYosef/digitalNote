83cb94d18207f6170fcc63cba6aa205b94747e98
Yosef <yosefplayz4580@gmail.com>
2023-06-25 21:20:41 +0300

    cool

--- client/src/comps/Dialogs/AddSelect.jsx
+++ client/src/comps/Dialogs/AddSelect.jsx
@@ -1,8 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { MyContext } from "../Context/Context";
-import { BsCreditCardFill } from "react-icons/bs";
+import { BsCreditCardFill, BsCashStack } from "react-icons/bs";
+import { GiPaperPlane } from "react-icons/gi";
+import { AiOutlineQuestion } from "react-icons/ai";
+import { FaMoneyCheck } from "react-icons/fa";
 
 export default function AddSelect() {
+  const [infoObject, setInfoObject] = useState([
+    {
+      text: "Cпособ Oплаты",
+      icon: <AiOutlineQuestion />,
+    },
+    {
+      text: "бит",
+      icon: <GiPaperPlane />,
+    },
+    {
+      text: "Наличные",
+      icon: <BsCashStack />,
+    },
+    {
+      text: "Kредитная Kарта",
+      icon: <BsCreditCardFill />,
+    },
+    {
+      text: "Чек ",
+      icon: <FaMoneyCheck />,
+    },
+  ]);
+
+  const [clickedIndex, setClickedIndex] = useState(0);
+
+  const getSelected = (index) => {
+    setClickedIndex(index);
+  };
+
   const { selectOpen, setSelectOpen } = useContext(MyContext);
   return (
     <div
@@ -11,23 +43,40 @@ export default function AddSelect() {
         selectOpen ? "back" : "close"
       }`}
     >
-      <div className="illustration">
-        <div
-          className="combine item flex jcac color gatherThem trans"
-        >
-          <BsCreditCardFill
-            style={{ filter: "drop-shadow(0 0 1vmin black)" }}
-          />
-          <div className="stand"></div>
+      <div className="illustration flex jcac">
+        <p className="title gradientText titleInfo">
+          {" "}
+          {infoObject[clickedIndex].text}
+        </p>
+        <div className="item flex jcac color gatherThem trans">
+          {infoObject[clickedIndex].icon}
         </div>
+        <div className="stand"></div>
       </div>
-      <div className="optionsArea">
+      <div className="optionsArea flex">
         <button
           onClick={() => setSelectOpen(false)}
           className="doneButton trans pointer"
         >
           закрывать
         </button>
+        <ul className="special">
+          {infoObject.map((info, index) => (
+            <li
+              key={index}
+              onClick={() => getSelected(index)}
+              className={
+                index === 0
+                  ? "flex jcac color title gradientText"
+                  : clickedIndex === index
+                  ? "flex jcac color pointer trans active"
+                  : "flex jcac color pointer trans"
+              }
+            >
+              {info.text}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
