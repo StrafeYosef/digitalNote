8d161393bb12e7739db49cc66d68a89915311149
Yosef <yosefplayz4580@gmail.com>
2023-07-05 21:16:43 +0300

    cool

--- server/controllers/Missions/postMission.js
+++ server/controllers/Missions/postMission.js
@@ -1,29 +1,41 @@
 const Mission = require("../../models/missionModel");
 const nodemailer = require("nodemailer");
-const { OAuth2Client } = require("google-auth-library");
-const axios = require("axios");
-let accessToken = null;
-let accessTokenExpiration = 0;
+const { google } = require("googleapis");
+
 require("dotenv").config();
-
-async function refreshAccessToken(refreshToken) {
-  const oAuth2Client = new OAuth2Client(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    "http://localhost:5174/missions/postMission"
-  );
-
-  try {
-    const { tokens } = await oAuth2Client.refreshToken(refreshToken);
-    return tokens.access_token;
-  } catch (error) {
-    console.error("Error refreshing access token:", error);
-    throw new Error("Error refreshing access token");
-  }
-}
 
 const postMission = async (req, res) => {
   const { first, email, second, third, date } = req.body;
+
+  const someValues = [
+    "להלהללה",
+    "קוסמטיקה רפואית - הסרת שיער בלייזר",
+    "טל': 050-123-1234 | אי מייל: akaa@.il",
+    `קבלה מספר 0001 /3`,
+    "הכדשכש 4214 הכגד 1234567",
+    `לכבוד ${first}`,
+    "",
+    "!תודה על בחירתכם בשירות שלנו",
+    ".אם יש לכם כל משוב או שאלה, אנא אל תהססו ליצור קשר עימי",
+    ",בברכה",
+    "להלהללה",
+  ];
+
+  const html = someValues.map((some, index) => {
+    if (index === 7 || index === 8 || index === 9) {
+      return `<table style="width: 100%; border-collapse: collapse; border: 1px solid black;">
+        <tr>
+          <td style="text-align: right; padding: 10px;">
+            <p style="font-size: 20px; font-weight: bold;"><strong>${some}</strong></p>
+          </td>
+        </tr>
+      </table>`;
+    } else {
+      return `<p style="font-size: 18px;">${some}</p>`;
+    }
+  });
+  
+
   try {
     if (
       !first ||
@@ -40,62 +52,61 @@ const postMission = async (req, res) => {
     const newMission = new Mission({ first, email, second, third, date });
     await newMission.save();
 
-    const transporter = nodemailer.createTransport({
-      service: "Gmail",
-      auth: {
-        type: "OAuth2",
-        user: process.env.EMAIL,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
-      },
+    const OAuth2Client = new google.auth.OAuth2(
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET,
+      process.env.REDIRECT_URI
+    );
+
+    OAuth2Client.setCredentials({
+      refresh_token: process.env.REFRESH_TOKEN,
     });
 
-    if (!accessToken || Date.now() / 1000 > accessTokenExpiration) {
+    async function sendMail() {
       try {
-        accessToken = await refreshAccessToken(process.env.REFRESH_TOKEN);
-        accessTokenExpiration = Date.now() / 1000 + 3600;
-        console.log("Access Token Refreshed:", accessToken);
-      } catch (error) {
-        return res.status(500).json({ err: "Error refreshing access token" });
-      }
-    }
+        const accessToken = await OAuth2Client.getAccessToken();
 
-    const tokenInfoUrl =
-      "https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=" +
-      accessToken;
-    try {
-      const response = await axios.get(tokenInfoUrl);
-      const tokenInfo = response.data;
-      console.log("Current Token Info:", tokenInfo);
-
-      if (tokenInfo.aud !== process.env.CLIENT_ID) {
-        console.error("Invalid access token");
-        return res.status(401).json({ err: "Invalid access token" });
-      }
-
-      const mailOptions = {
-        from: process.env.EMAIL,
-        to: email,
-        subject: "Welcome to MyApp",
-        text: `Welcome to MyApp!`,
-      };
-
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.error("Error occurred while sending the email:", error);
-          return res.status(500).json({ err: "Error sending email" });
-        } else {
-          console.log("Email sent successfully!", info);
-          return res
-            .status(200)
-            .json({ msg: "Mission and email added successfully" });
+        if (OAuth2Client.isTokenExpiring()) {
+          const { res: refreshResponse } =
+            await OAuth2Client.refreshAccessToken();
+          accessToken = refreshResponse.access_token;
         }
-      });
-    } catch (error) {
-      console.error("Error checking token validity:", error);
-      return res.status(500).json({ err: "Error checking token validity" });
+
+        const transport = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            type: "OAuth2",
+            user: process.env.EMAIL,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            refreshToken: process.env.REFRESH_TOKEN,
+            accessToken,
+          },
+        });
+
+        const mailOptions = {
+          from: process.env.EMAIL,
+          to: email,
+          subject: "תודה רבה שבחרתם בקוסמטיקה רפואית - הסרת שיער בלייזר",
+          html: html.toString(),
+        };
+
+        const result = await transport.sendMail(mailOptions);
+        return result;
+      } catch (error) {
+        throw new Error("Error sending email: " + error.message);
+      }
     }
+
+    sendMail()
+      .then((result) => {
+        console.log("Email sent!", result);
+        return res.status(200).json({ message: "Email sent successfully!" });
+      })
+      .catch((err) => {
+        console.log(err.message);
+        return res.status(500).json({ error: err.message });
+      });
   } catch (error) {
     return res.status(500).json(error);
   }
